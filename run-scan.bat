bb92885c72abc615bc22f737a2393641d63be1fa
Unise0015 <unise051@gmail.com>
2026-08-08 15:42:58 +0530

    docs: remove dev scripts and sample files from repository

--- run-scan.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Batch script to run the PULSE Security Scanner CLI within the virtual environment
-if exist "%~dp0venv\Scripts\pulse.exe" (
-    "%~dp0venv\Scripts\pulse.exe" %*
-) else (
-    echo Error: Virtual environment or pulse executable not found.
-    echo Please run python -m venv venv and pip install -e .
-    exit /b 1
-)
