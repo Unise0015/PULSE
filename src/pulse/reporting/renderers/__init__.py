a5f73bcf8f88a0b4d613e9e61aeb35393b9f367e
Unise0015 <unise051@gmail.com>
2026-08-21 22:00:41 +0530

    fix: import TextRenderer in reporting renderers init

--- src/pulse/reporting/renderers/__init__.py
+++ src/pulse/reporting/renderers/__init__.py
@@ -4,6 +4,7 @@ from pulse.reporting.renderers.base import BaseRenderer
 from pulse.reporting.renderers.json_renderer import JSONRenderer
 from pulse.reporting.renderers.markdown_renderer import MarkdownRenderer
 from pulse.reporting.renderers.html_renderer import HTMLRenderer
+from pulse.reporting.renderers.text_renderer import TextRenderer
 
 __all__ = [
     "BaseRenderer",
